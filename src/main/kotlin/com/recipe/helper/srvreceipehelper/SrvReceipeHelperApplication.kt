a8173ab2f4100c338426ed35ac02af7f83ff2fe9
Alex Travagin Chatalov <alex.chatalov@ifood.com.br>
2023-11-29 15:34:40 -0300

    initial commit

---
+++ src/main/kotlin/com/recipe/helper/srvreceipehelper/SrvReceipeHelperApplication.kt
@@ -0,0 +1,11 @@
+package com.recipe.helper.srvreceipehelper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SrvReceipeHelperApplication
+
+fun main(args: Array<String>) {
+	runApplication<SrvReceipeHelperApplication>(*args)
+}
---
+++ src/test/kotlin/com/recipe/helper/srvreceipehelper/SrvReceipeHelperApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.recipe.helper.srvreceipehelper
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SrvReceipeHelperApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
